b26652b6f59a4992f2ced201d8ae89849d7d7f2e
Andrés Jacinto <andres@fatrian.com>
2022-02-09 15:15:49 -0600

    progreso

---
+++ src/LayerApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LayerApp = () => {
+  return <>
+    <h1>Infra Teatro</h1>
+    <h2>Feel good hits of the summer</h2>
+
+    <div id='movieFrame'>
+        <img src='https://lh3.googleusercontent.com/pAjHitfz1-laHZuBUXqpC499PqF3jvxU_OCYwkam0qduLLo5XSPDA2m1fT797aAVuH-vvCIDQerAPU_jtqaHNUbpu4Sji2zj_A=w960-rj-nu-e365'/>
+        <p>PIED PIPER</p>
+    </div>
+  </>;
+};
+
+export default LayerApp;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import {info} from '../src/ids'
+import ReactDOM from "react-dom";
+import LayerApp from "./LayerApp"
+import "../src/index.css"
+
+const divRoot = document.querySelector("#root");
 
 info.forEach(title => {
+  
   const paragraph = document.createElement('p');
   paragraph.innerText = title;
 
-  document.body.append(paragraph);
+  // document.body.append(paragraph);
+  ReactDOM.render(<LayerApp />, divRoot);
 });
